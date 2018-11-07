e5960300bc8e393116f16b140f4db84c5ed33abf
terryl <1127027668@qq.com>
2018-11-07 10:49:45 +0800

    init2

---
+++ index.js
@@ -0,0 +1,8 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+//import TabNavigator from ‘react-native-tab-navigator’;
+
+AppRegistry.registerComponent(appName, () => App);
